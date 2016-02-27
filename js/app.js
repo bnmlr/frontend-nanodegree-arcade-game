d170b0306d7cbe77e9e770a4c43feb7e3155dff5
Ben Miller <Ben Miller>
2016-02-27 10:09:28 -0700

    Make enemies appear and move

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.x = x;
+    this.y = y;
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -14,6 +16,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = (this.x + 1);
+    //this.x = this.x + 1
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,15 +25,39 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x, y) {
+	this.sprite = 'images/char-boy.png'
+};
 
+Player.prototype.update = function(dt) {
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function() {
+    
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+//var player = new Player(50, 50);
+//player.render();
 
+
+//need to figure out the right x and y coordinates for the 3 stone rows to assign right coords for the enemies?
+var enemy1 = new Enemy(0, 60);
+var enemy2 = new Enemy(0, 145);
+var enemy3 = new Enemy(0, 230);
+var allEnemies = [];
+allEnemies.push(enemy1, enemy2, enemy3);
 
 
 // This listens for key presses and sends the keys to your
